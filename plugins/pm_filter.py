7954f876319fec6a0172c4869cb6898908220e4e
Arvikkumar <162270089+Arvikkumar@users.noreply.github.com>
2024-03-28 18:28:32 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -90,7 +90,7 @@ async def pm_spoll_tester(bot, query):
         k = (movie, files, offset, total_results)
         await pm_AutoFilter(bot, query, k)
     else:
-        k = await query.message.edit('Tʜɪs Mᴏᴠɪᴇ Nᴏᴛ Fᴏᴜɴᴅ Iɴ Dᴀᴛᴀʙᴀsᴇ')
+        k = await query.message.edit('𝙏𝙝𝙞𝙨 𝙚𝙗𝙤𝙤𝙠 𝙞𝙨𝙣'𝙩 𝙖𝙫𝙖𝙞𝙡𝙖𝙗𝙡𝙚 𝙧𝙞𝙜𝙝𝙩 𝙣𝙤𝙬')
         await asyncio.sleep(10)
         await k.delete()
 
