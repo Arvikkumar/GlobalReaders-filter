1b824e8676dbe18e8e17cc671585d3326866801d
Arvikkumar <162270089+Arvikkumar@users.noreply.github.com>
2024-03-28 13:46:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -56,7 +56,7 @@ SHORT_URL = environ.get("SHORT_URL")
 SHORT_API = environ.get("SHORT_API")
 
 # Others
-IMDB_DELET_TIME = int(environ.get('IMDB_DELET_TIME', "60"))
+IMDB_DELET_TIME = int(environ.get('IMDB_DELET_TIME', "300"))
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL','-1002039066656'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'GlobalReaders09')
 P_TTI_SHOW_OFF = is_enabled(environ.get('P_TTI_SHOW_OFF', "True"), True)
